55138f41412aae02ae920f5fdcae945332e37c63
Kawe Maximo Godoi <kawemaximogodoi@hotmail.com>
2023-09-19 12:45:38 -0300

    FORMs

---
+++ form/dados-cadastrais/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="pt-br"> <!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width" />
+    <title>Artec Solar</title>
+    <!--
+    Desenvolvido por Relev Tecnologia 
+    http://www.relev.com.br 
+-->
+
+    <!-- css -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/bootstrap-responsive.min.css">
+    <link rel="stylesheet" href="../../css/main.css">
+    <link rel="shortcut icon" type="image/png" href="../../images/logo.jpg">
+    <meta name="description" content="Contato.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Artec Solar" name="keywords">
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+    <!-- js -->
+    <script src="../../js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    <script src="../../js/jquery-1.9.1.min.js"></script>
+    <script src="../../js/bootstrap.min.js"></script>
+    <script src="../../js/main.js"></script>
+</head>
+
+<body>
+    <header class="navbar navbar-fixed-top">
+        <div class="navbar-inner">
+            <div class="container">
+                <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a>
+                <a id="logo" class="pull-left" href="index.html"></a>
+                <div class="nav-collapse collapse pull-right">
+                    <ul class="nav">
+                        <li><a href="../../index.html">Home</a></li>
+                        <li><a href="../../empresa.html">Empresa</a></li>
+                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Produtos <i
+                                    class="fa fa-angle-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="../../produto-01.html">Energia Solar</a></li>
+                                <li><a href="../../produto-02.html">Aquecimento Solar</a></li>
+                                <li><a href="../../produto-03.html">Climatização</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Serviços <i
+                                    class="fa fa-angle-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="../../servico-01.html">Higienização de Ar Condicionado</a></li>
+                                <li><a href="../../servico-02.html">Manutenção de Aquecedor Solar</a></li>
+                                <li><a href="../../servico-03.html">Manutenção de Ar Condicionado</a></li>
+                                <li><a href="../../produto-01.html">Projeto de Energia Fotovoltaica</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="../../contato.html">Contato</a></li>
+                        <li class="login"><a href="http://webmail.artecsolar.com.br" target="_blank"
+                                title="Acessar WebMail"><i class="fa fa-envelope"></i></a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </div>
+    </header>
+    <br>
+    <br>
+    <section id="contact-page" class="container">
+        <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSdn7gWHTKLupjXQSngLCGlmfe9lRzElJjc4x_kI8ZuRv7MyQA/viewform?embedded=true"
+            width="100%" height="775pm" frameborder="0" marginheight="0" marginwidth="0">Carregando…</iframe>
+    </section>
+</body>
+</html>
