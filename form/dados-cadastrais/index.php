c3e9cbc14d83700caeaa3dea79e564d6a8f3971d
Kawe Maximo Godoi <kawemaximogodoi@hotmail.com>
2023-09-19 17:37:54 -0300

    FORMs

--- form/dados-cadastrais/index.php
+++ form/dados-cadastrais/index.php
@@ -45,7 +45,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </a>
-                <a id="logo" class="pull-left" href="index.html"></a>
+                <a id="logo" class="pull-left" href="../../index.html"></a>
                 <div class="nav-collapse collapse pull-right">
                     <ul class="nav">
                         <li><a href="../../index.html">Home</a></li>
